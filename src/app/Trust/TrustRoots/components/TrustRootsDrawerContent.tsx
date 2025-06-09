a63bd836caec39da18e903f1a08829ce74abf7c4
kahboom <rachelyordan@gmail.com>
2025-06-09 21:24:30 +0100

    feat(ui): add trust root info tab

--- src/app/Trust/TrustRoots/components/TrustRootsDrawerContent.tsx
+++ src/app/Trust/TrustRoots/components/TrustRootsDrawerContent.tsx
@@ -1,10 +1,26 @@
 import { Fragment } from 'react';
-import { Flex, FlexItem, Icon, Pagination, Tab, Tabs, TabTitleText } from '@patternfly/react-core';
-import { Table, Tbody, Td, Th, Thead, Tr } from '@patternfly/react-table';
+import {
+  Button,
+  DescriptionList,
+  DescriptionListDescription,
+  DescriptionListGroup,
+  DescriptionListTermHelpText,
+  DescriptionListTermHelpTextButton,
+  Flex,
+  FlexItem,
+  Icon,
+  PageSection,
+  Pagination,
+  Popover,
+  Tab,
+  Tabs,
+  TabTitleText,
+} from '@patternfly/react-core';
+import { Caption, Table, Tbody, Td, Th, Thead, Tr } from '@patternfly/react-table';
 import CheckCircleIcon from '@patternfly/react-icons/dist/esm/icons/check-circle-icon';
 import { exampleTrustRoot } from '../TrustRoots.data';
 import { capitalizeFirstLetter, formatDate } from '@app/utils/utils';
-import { ExclamationTriangleIcon } from '@patternfly/react-icons';
+import { ExclamationTriangleIcon, PlusCircleIcon } from '@patternfly/react-icons';
 
 const TrustRootsDrawerContent = () => {
   return (
@@ -91,7 +107,76 @@ const TrustRootsDrawerContent = () => {
           <Pagination itemCount={exampleTrustRoot.certificates.length} perPage={10} page={1} variant="bottom" />
         </Tab>
         <Tab eventKey={2} title={<TabTitleText>Info</TabTitleText>}>
-          More info about the Trust Root
+          <PageSection>
+            <DescriptionList>
+              <DescriptionListGroup>
+                <DescriptionListTermHelpText>
+                  <Popover headerContent={<div>ID</div>} bodyContent={<div>Additional info</div>}>
+                    <DescriptionListTermHelpTextButton> ID </DescriptionListTermHelpTextButton>
+                  </Popover>
+                </DescriptionListTermHelpText>
+                <DescriptionListDescription>{exampleTrustRoot.id}</DescriptionListDescription>
+              </DescriptionListGroup>
+              <DescriptionListGroup>
+                <DescriptionListTermHelpText>
+                  <Popover headerContent={<div>Source</div>} bodyContent={<div>Additional source info</div>}>
+                    <DescriptionListTermHelpTextButton> Source </DescriptionListTermHelpTextButton>
+                  </Popover>
+                </DescriptionListTermHelpText>
+                <DescriptionListDescription>
+                  <a href="#">{exampleTrustRoot.source}</a>
+                </DescriptionListDescription>
+              </DescriptionListGroup>
+              <DescriptionListGroup>
+                <DescriptionListTermHelpText>
+                  <Popover headerContent={<div>Policy URL</div>} bodyContent={<div>Additional info</div>}>
+                    <DescriptionListTermHelpTextButton> Policy URL </DescriptionListTermHelpTextButton>
+                  </Popover>
+                </DescriptionListTermHelpText>
+                <DescriptionListDescription>{exampleTrustRoot.policyUrl}</DescriptionListDescription>
+              </DescriptionListGroup>
+              <DescriptionListGroup>
+                <DescriptionListTermHelpText>
+                  <Popover headerContent={<div>Last Updated</div>} bodyContent={<div>Additional info</div>}>
+                    <DescriptionListTermHelpTextButton> Last Updated </DescriptionListTermHelpTextButton>
+                  </Popover>
+                </DescriptionListTermHelpText>
+                <DescriptionListDescription>{formatDate(exampleTrustRoot.lastUpdated)}</DescriptionListDescription>
+              </DescriptionListGroup>
+              <DescriptionListGroup>
+                <DescriptionListTermHelpText>
+                  <Popover headerContent={<div>Type</div>} bodyContent={<div>Additional info</div>}>
+                    <DescriptionListTermHelpTextButton> Type </DescriptionListTermHelpTextButton>
+                  </Popover>
+                </DescriptionListTermHelpText>
+                <DescriptionListDescription>
+                  <Button variant="link" isInline icon={<PlusCircleIcon />}>
+                    {exampleTrustRoot.type}
+                  </Button>
+                </DescriptionListDescription>
+              </DescriptionListGroup>
+              <DescriptionListGroup>
+                <DescriptionListTermHelpText>
+                  <Popover headerContent={<div>Certificates</div>} bodyContent={<div>Additional info</div>}>
+                    <DescriptionListTermHelpTextButton> Certificates </DescriptionListTermHelpTextButton>
+                  </Popover>
+                </DescriptionListTermHelpText>
+                <DescriptionListDescription>
+                  {exampleTrustRoot.certificates.length} Certificate(s)
+                </DescriptionListDescription>
+              </DescriptionListGroup>
+              <DescriptionListGroup>
+                <DescriptionListTermHelpText>
+                  <Popover headerContent={<div>TUF Metadata</div>} bodyContent={<div>Additional info</div>}>
+                    <DescriptionListTermHelpTextButton> TUF Metadata </DescriptionListTermHelpTextButton>
+                  </Popover>
+                </DescriptionListTermHelpText>
+                <DescriptionListDescription>
+                  {exampleTrustRoot.tufMetadata.length} Metadata File(s)
+                </DescriptionListDescription>
+              </DescriptionListGroup>
+            </DescriptionList>
+          </PageSection>
         </Tab>
       </Tabs>
     </Fragment>
