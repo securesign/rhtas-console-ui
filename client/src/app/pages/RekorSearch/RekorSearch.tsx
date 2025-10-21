56e6c127980e8d194736ec4eeaae360e8724ef5e
Carlos Feria <carlosthe19916@gmail.com>
2025-10-21 13:23:59 +0200

    fix: add page header to rekor search ui (#67)
    Signed-off-by: Carlos Feria <2582866+carlosthe19916@users.noreply.github.com>

--- client/src/app/pages/RekorSearch/RekorSearch.tsx
+++ client/src/app/pages/RekorSearch/RekorSearch.tsx
@@ -1,13 +1,24 @@
-import { PageSection } from "@patternfly/react-core";
-import { Explorer } from "./components/Explorer";
+import React, { Fragment } from "react";
+
 import { RekorClientProvider } from "@app/api/context";
+import { Content, PageSection } from "@patternfly/react-core";
+
+import { Explorer } from "./components/Explorer";
 
 export const RekorSearch: React.FC = () => {
   return (
-    <PageSection>
-      <RekorClientProvider>
-        <Explorer />
-      </RekorClientProvider>
-    </PageSection>
+    <Fragment>
+      <PageSection variant="default">
+        <Content>
+          <h1>Rekor Search</h1>
+          <p>Search the Rekor public transparency log.</p>
+        </Content>
+      </PageSection>
+      <PageSection variant="secondary" isFilled>
+        <RekorClientProvider>
+          <Explorer />
+        </RekorClientProvider>
+      </PageSection>
+    </Fragment>
   );
 };
