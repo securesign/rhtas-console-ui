f190a19a88b5c8c1224b538dacff0f39c2536994
kahboom <rachelyordan@gmail.com>
2025-10-27 14:30:05 +0000

    feat(artifacts): add route and setup component
    feat(artifacts): update mock and query
    
    feat(artifact): add form control and field validation
    
    fix: re-add hook state
    
    fix(ci): type issues with react-syntax-highlighter
    
    enhancement(artifact): improve search results
    
    enhancement(artifact): add metadata accordion
    
    fix: use paths constant for route
    
    fix: lazy load artifacts
    
    chore: remove accordion, use content for titles
    
    fix: add type for artifact uri
    
    fix: fallback for uri type assertion
    
    chore: remove gcTime setting
    
    fix: remove unnecessary query dependency

--- client/src/app/Routes.tsx
+++ client/src/app/Routes.tsx
@@ -5,18 +5,21 @@ import { Navigate, useRoutes } from "react-router-dom";
 import { Bullseye, Spinner } from "@patternfly/react-core";
 import { ErrorFallback } from "./components/ErrorFallback";
 
+const Artifacts = lazy(() => import("./pages/Artifacts"));
 const TrustRoot = lazy(() => import("./pages/TrustRoot"));
 const RekorSearch = lazy(() => import("./pages/RekorSearch"));
 
 export const Paths = {
-  trustRoot: "/trust-root",
+  artifacts: "/artifacts",
   rekorSearch: "/rekor-search",
+  trustRoot: "/trust-root",
 } as const;
 
 export const AppRoutes = () => {
   const allRoutes = useRoutes([
     { path: "/", element: <Navigate to={Paths.trustRoot} /> },
     { path: Paths.trustRoot, element: <TrustRoot /> },
+    { path: Paths.artifacts, element: <Artifacts /> },
     { path: Paths.rekorSearch, element: <RekorSearch /> },
   ]);
 
