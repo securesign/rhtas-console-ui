0c3f7d35b734f2bd786ca88a794773341253fcad
kahboom <rachelyordan@gmail.com>
2025-10-30 14:44:00 +0000

    chore: remove accordion, use content for titles

--- client/src/app/pages/Artifacts/components/ArtifactResults.tsx
+++ client/src/app/pages/Artifacts/components/ArtifactResults.tsx
@@ -1,13 +1,11 @@
-import { Fragment, useState } from "react";
+import { Fragment } from "react";
 import type { PrismTheme } from "types/prism-theme";
 import type { ImageMetadataResponse } from "@app/client";
 import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionToggle,
   Card,
   CardBody,
+  Content,
+  ContentVariants,
   Divider,
   Flex,
   FlexItem,
@@ -23,127 +21,103 @@ export interface IArtifactResults {
 }
 
 export const ArtifactResults = ({ artifact }: IArtifactResults) => {
-  type PanelId = "metadata-content";
-  const [expanded, setExpanded] = useState<PanelId[]>([]);
-
-  const toggle = (id: PanelId) => {
-    const index = expanded.indexOf(id);
-    const newExpanded: PanelId[] =
-      index >= 0 ? [...expanded.slice(0, index), ...expanded.slice(index + 1, expanded.length)] : [...expanded, id];
-    setExpanded(newExpanded);
-  };
-
   return (
     <div style={{ margin: "2em auto" }}>
       <p>Showing 1 of 1</p>
       <Card style={{ margin: "1.5em auto 2em", overflowY: "hidden" }}>
         <CardBody>
-          <h2 style={{ margin: "1.25em auto", overflow: "hidden", textOverflow: "ellipsis" }}>
-            <b>Artifact:</b> {artifact.image}
-          </h2>
+          <Content
+            component={ContentVariants.h4}
+            style={{ margin: "1.25em auto", overflow: "hidden", textOverflow: "ellipsis" }}
+          >
+            Artifact: {artifact.image}
+          </Content>
           <Divider />
           <Panel style={{ marginTop: "1.25em" }}>
-            <h5 style={{ margin: "1em auto" }}>Digest</h5>
+            <Content component={ContentVariants.h6} style={{ margin: "1em auto" }}>
+              Digest
+            </Content>
             <SyntaxHighlighter language="text" style={darkTheme as unknown as PrismTheme}>
               {artifact.digest}
             </SyntaxHighlighter>
           </Panel>
           <Panel style={{ margin: "0.75em auto" }}>
             <Fragment>
-              <Accordion>
-                <>
-                  <AccordionItem isExpanded={expanded.includes("metadata-content")}>
-                    <AccordionToggle
-                      id={"metadata-header"}
-                      onClick={() => {
-                        toggle("metadata-content");
-                      }}
-                    >
-                      <b>Metadata</b>
-                    </AccordionToggle>
-                    <AccordionContent>
-                      <Grid hasGutter={true}>
-                        <GridItem sm={3}>
-                          <Flex style={{ padding: "1em" }}>
-                            <Divider orientation={{ default: "vertical" }} style={{ margin: "inherit 1em" }} />
-                            <FlexItem>
-                              <h3>Media Type</h3>
-                              <p
-                                style={{
-                                  overflow: "hidden",
-                                  textOverflow: "ellipsis",
-                                  display: "flex",
-                                  alignItems: "center",
-                                  justifyContent: "start",
-                                }}
-                              >
-                                {artifact.metadata.mediaType}
-                              </p>
-                            </FlexItem>
-                          </Flex>
-                        </GridItem>
-                        <GridItem sm={3}>
-                          <Flex style={{ padding: "1em" }}>
-                            <Divider orientation={{ default: "vertical" }} style={{ margin: "inherit 1em" }} />
-                            <FlexItem>
-                              <h3>Size</h3>
-                              <p
-                                style={{
-                                  overflow: "hidden",
-                                  textOverflow: "ellipsis",
-                                  display: "flex",
-                                  alignItems: "center",
-                                  justifyContent: "start",
-                                }}
-                              >
-                                {artifact.metadata.size}
-                              </p>
-                            </FlexItem>
-                          </Flex>
-                        </GridItem>
-                        <GridItem sm={3}>
-                          <Flex style={{ padding: "1em" }}>
-                            <Divider orientation={{ default: "vertical" }} style={{ margin: "inherit 1em" }} />
-                            <FlexItem>
-                              <h3>Created</h3>
-                              <p
-                                style={{
-                                  overflow: "hidden",
-                                  textOverflow: "ellipsis",
-                                  display: "flex",
-                                  alignItems: "center",
-                                  justifyContent: "start",
-                                }}
-                              >
-                                {artifact.metadata.created}
-                              </p>
-                            </FlexItem>
-                          </Flex>
-                        </GridItem>
-                        <GridItem sm={3}>
-                          <Flex style={{ padding: "1em" }}>
-                            <Divider orientation={{ default: "vertical" }} style={{ margin: "inherit 1em" }} />
-                            <FlexItem>
-                              <h3>Labels</h3>
-                              <p
-                                style={{
-                                  overflow: "hidden",
-                                  textOverflow: "ellipsis",
-                                  display: "flex",
-                                  alignItems: "center",
-                                  justifyContent: "start",
-                                }}
-                              >
-                                {artifact.metadata.labels?.maintainer}
-                              </p>
-                            </FlexItem>
-                          </Flex>
-                        </GridItem>
-                      </Grid>
-                    </AccordionContent>
-                  </AccordionItem>
-                </>
-              </Accordion>
+              <Grid>
+                <GridItem span={4}>
+                  <Flex style={{ padding: "1em" }}>
+                    <FlexItem>
+                      <Content component={ContentVariants.h6}>Media Type</Content>
+                      <p
+                        style={{
+                          overflow: "hidden",
+                          textOverflow: "ellipsis",
+                          display: "flex",
+                          alignItems: "center",
+                          justifyContent: "start",
+                        }}
+                      >
+                        {artifact.metadata.mediaType}
+                      </p>
+                    </FlexItem>
+                  </Flex>
+                </GridItem>
+                <GridItem span={2}>
+                  <Flex style={{ padding: "1em" }}>
+                    <FlexItem>
+                      <Content component={ContentVariants.h6}>Size</Content>
+                      <p
+                        style={{
+                          overflow: "hidden",
+                          textOverflow: "ellipsis",
+                          display: "flex",
+                          alignItems: "center",
+                          justifyContent: "start",
+                        }}
+                      >
+                        {artifact.metadata.size}
+                      </p>
+                    </FlexItem>
+                  </Flex>
+                </GridItem>
+                <GridItem span={3}>
+                  <Flex style={{ padding: "1em" }}>
+                    <FlexItem>
+                      <Content component={ContentVariants.h6}>Created</Content>
+                      <p
+                        style={{
+                          overflow: "hidden",
+                          textOverflow: "ellipsis",
+                          display: "flex",
+                          alignItems: "center",
+                          justifyContent: "start",
+                        }}
+                      >
+                        {artifact.metadata.created}
+                      </p>
+                    </FlexItem>
+                  </Flex>
+                </GridItem>
+                <GridItem span={3}>
+                  <Flex style={{ padding: "1em" }}>
+                    <Divider orientation={{ default: "vertical" }} style={{ margin: "auto 1em" }} />
+                    <FlexItem>
+                      <Content component={ContentVariants.h6}>Labels</Content>
+                      <p
+                        style={{
+                          overflow: "hidden",
+                          textOverflow: "ellipsis",
+                          display: "flex",
+                          alignItems: "center",
+                          justifyContent: "start",
+                        }}
+                      >
+                        {artifact.metadata.labels?.maintainer}
+                      </p>
+                    </FlexItem>
+                  </Flex>
+                </GridItem>
+              </Grid>
             </Fragment>
           </Panel>
         </CardBody>
