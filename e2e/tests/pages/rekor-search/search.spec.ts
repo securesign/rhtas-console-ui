ad8f1fddae153bc9b356137ede1dcd918b07d10d
Carlos Feria <2582866+carlosthe19916@users.noreply.github.com>
2025-11-26 21:35:08 -0500

    tests: add rekor search playwright basic tests
    Signed-off-by: Carlos Feria <2582866+carlosthe19916@users.noreply.github.com>

---
+++ e2e/tests/pages/rekor-search/search.spec.ts
@@ -0,0 +1,49 @@
+import { expect, Page, test } from "@playwright/test";
+
+import { Navigation } from "../../common/Navigation";
+import { RekorSearchPage } from "./RekorSearchPage";
+
+const verify_search_page_has_main_sections = async (page: Page) => {
+  // Verify main sections were rendered
+  await expect(page.getByRole("heading", { name: "Hash" }).first()).toBeVisible();
+  await expect(page.getByRole("heading", { name: "Signature" }).first()).toBeVisible();
+  await expect(page.getByRole("heading", { name: "Public Key Certificate" }).first()).toBeVisible();
+};
+
+test.describe("Rekor Search UI", () => {
+  test.beforeEach(async ({ page }) => {
+    const navigation = Navigation.build(page);
+    await navigation.goToSidebar("Rekor Search");
+  });
+
+  test("Search by email", async ({ page }) => {
+    const rekorSearchPage = await RekorSearchPage.build(page);
+    await rekorSearchPage.search("Email", "carlosthe19916@gmail.com");
+
+    await verify_search_page_has_main_sections(page);
+  });
+
+  test("Search by hash", async ({ page }) => {
+    const rekorSearchPage = await RekorSearchPage.build(page);
+    await rekorSearchPage.search("Hash", "sha256:1174c40f437d2cbd0d7d14fba2b030bf5595d63e66c26e784b764ba4a0bd6780");
+
+    await verify_search_page_has_main_sections(page);
+  });
+
+  test("Search by entry uuid", async ({ page }) => {
+    const rekorSearchPage = await RekorSearchPage.build(page);
+    await rekorSearchPage.search(
+      "Entry UUID",
+      "108e9186e8c5677a023bef8fb7d3eb499b3df47f26592175c2fd51442d6e14a45591b1e5d4dcff24"
+    );
+
+    await verify_search_page_has_main_sections(page);
+  });
+
+  test("Search by log index", async ({ page }) => {
+    const rekorSearchPage = await RekorSearchPage.build(page);
+    await rekorSearchPage.search("Log Index", "380502080");
+
+    await verify_search_page_has_main_sections(page);
+  });
+});
