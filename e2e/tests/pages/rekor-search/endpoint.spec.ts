15942192da4ee51cffcd9db647e31e05214f7c79
Carlos Feria <carlosthe19916@gmail.com>
2025-12-16 16:14:33 -0500

    tests: migrate cypres tests from rekor-search-ui (#92)
    Signed-off-by: Carlos Feria <2582866+carlosthe19916@users.noreply.github.com>

---
+++ e2e/tests/pages/rekor-search/endpoint.spec.ts
@@ -0,0 +1,28 @@
+import { expect, test } from "@playwright/test";
+
+import { RekorSearchPage } from "./RekorSearchPage";
+
+test.describe("Overriding the Rekor Endpoint", () => {
+  test("Should use the fallback endpoint if one is not provided", async ({ page }) => {
+    const rekorSearchPage = await RekorSearchPage.build(page);
+    await rekorSearchPage.applySearch("Email", "carlosthe19916@gmail.com");
+
+    // Open settings modal
+    await page.getByTestId("settings-button").click();
+
+    // Verify default values
+    await expect(page.locator("#rekor-endpoint-override")).toHaveValue("https://rekor.sigstore.dev");
+
+    // Close settings modal
+    await page.getByTestId("settings-close-button").click();
+
+    // Verify search
+    const response = await page.waitForResponse((response) => {
+      return response.url().includes("https://rekor.sigstore.dev") && response.request().method() === "GET";
+    });
+
+    await rekorSearchPage.applySearch("Email", "bob.callaway@gmail.com");
+
+    expect(response.url()).toContain("https://rekor.sigstore.dev");
+  });
+});
