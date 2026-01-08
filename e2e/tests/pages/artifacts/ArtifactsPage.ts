7a79d71bfd06eb57c0dbb5b12a32a98627f6152a
Carlos Feria <2582866+carlosthe19916@users.noreply.github.com>
2026-01-08 17:32:21 +0100

    tests: add playwright tests for artifacts
    Signed-off-by: Carlos Feria <2582866+carlosthe19916@users.noreply.github.com>

---
+++ e2e/tests/pages/artifacts/ArtifactsPage.ts
@@ -0,0 +1,39 @@
+import type { Page } from "@playwright/test";
+import { expect } from "../../assertions";
+import { Navigation } from "../../common/Navigation";
+import { Tabs } from "../../common/Tabs";
+
+export class ArtifactsPage {
+  private readonly _page: Page;
+  private readonly _tabs: Tabs<["Signatures", "Attestations"]>;
+
+  private constructor(page: Page) {
+    this._page = page;
+    this._tabs = Tabs.build(page, ["Signatures", "Attestations"]);
+  }
+
+  static async build(page: Page) {
+    const navigation = Navigation.build(page);
+    await navigation.goToSidebar("Artifacts");
+
+    return new ArtifactsPage(page);
+  }
+
+  getTabs() {
+    return this._tabs;
+  }
+
+  async searchArtifact(uri: string) {
+    await this._page.getByLabel("uri input field").fill(uri);
+    await this._page.getByRole("button", { name: "Search" }).click();
+
+    // Wait for results
+    await expect(this._page.locator(".pf-v6-c-card__header", { hasText: "Artifact:" })).toBeVisible({ timeout: 15000 });
+  }
+
+  async getArtifactCardHeader() {
+    const cardHeader = this._page.locator(".pf-v6-c-card__header", { hasText: "Artifact:" });
+    await expect(cardHeader).toBeVisible();
+    return cardHeader;
+  }
+}
