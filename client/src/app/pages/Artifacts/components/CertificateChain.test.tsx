ba93ae87a0f73f2ac1a658acb1a8c8c73439e4f0
Stanislav Semeniuk <ssemeniu@redhat.com>
2026-01-21 12:39:10 +0100

    fix: prop changes in CertificateChain test file

--- client/src/app/pages/Artifacts/components/CertificateChain.test.tsx
+++ client/src/app/pages/Artifacts/components/CertificateChain.test.tsx
@@ -3,7 +3,7 @@ import { render, screen } from "@testing-library/react";
 import { CertificateChain } from "./CertificateChain";
 
 test("renders certificate chain heading", () => {
-  render(<CertificateChain certificateChain={[]} />);
+  render(<CertificateChain certificateChain={[]} status="verified" />);
 
   expect(screen.getByText("Certificate Chain")).toBeInTheDocument();
 });
