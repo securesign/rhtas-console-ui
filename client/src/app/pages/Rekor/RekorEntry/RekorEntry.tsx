12f46fb0e6f23a716dacc1ca7829ac8c0589dacc
Stanislav Semeniuk <ssemeniu@redhat.com>
2026-03-30 11:19:42 +0200

    feat: added a back button to breadcrumbs

--- client/src/app/pages/Rekor/RekorEntry/RekorEntry.tsx
+++ client/src/app/pages/Rekor/RekorEntry/RekorEntry.tsx
@@ -1,15 +1,27 @@
 import React from "react";
 
 import { RekorClientProvider } from "@app/pages/Rekor/shared/utils/rekor/api/context";
-import { Alert, Breadcrumb, BreadcrumbItem, Content, PageSection, Spinner } from "@patternfly/react-core";
+import {
+  Alert,
+  Breadcrumb,
+  BreadcrumbItem,
+  Button,
+  Content,
+  Flex,
+  FlexItem,
+  PageSection,
+  Spinner,
+} from "@patternfly/react-core";
 
 import { DocumentMetadata } from "@app/components/DocumentMetadata";
 import { Paths, useRouteParams } from "@app/Routes";
 import { useFetchRekorEntry } from "@app/queries/rekor-search";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Entry } from "./components/Entry";
+import { ArrowLeftIcon } from "@patternfly/react-icons";
 
 const RekorEntryContent: React.FC = () => {
+  const navigate = useNavigate();
   const logIndex = useRouteParams("logIndex");
 
   const { data: entry, isLoading: loading, error } = useFetchRekorEntry(logIndex);
@@ -18,12 +30,21 @@ const RekorEntryContent: React.FC = () => {
     <>
       <DocumentMetadata title="Rekor Entry" />
       <PageSection type="breadcrumb">
-        <Breadcrumb>
-          <BreadcrumbItem>
-            <Link to={Paths.rekorSearch}>Logs</Link>
-          </BreadcrumbItem>
-          <BreadcrumbItem isActive>{logIndex}</BreadcrumbItem>
-        </Breadcrumb>
+        <Flex spaceItems={{ default: "spaceItemsXs" }}>
+          <FlexItem>
+            <Button size="sm" variant="plain" onClick={() => void navigate(-1)} style={{ cursor: "pointer" }}>
+              <ArrowLeftIcon />
+            </Button>
+          </FlexItem>
+          <FlexItem>
+            <Breadcrumb>
+              <BreadcrumbItem>
+                <Link to={Paths.rekorSearch}>Logs</Link>
+              </BreadcrumbItem>
+              <BreadcrumbItem isActive>{logIndex}</BreadcrumbItem>
+            </Breadcrumb>
+          </FlexItem>
+        </Flex>
       </PageSection>
       <PageSection>
         <Content>
