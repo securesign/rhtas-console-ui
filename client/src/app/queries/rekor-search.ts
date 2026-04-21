d925198ac0c4f61e678fc092c16506f3cda715e2
Stanislav Semeniuk <ssemeniu@redhat.com>
2026-04-21 09:54:36 +0200

    fix: added rekor search base url as a dependency for rekor search queries

--- client/src/app/queries/rekor-search.ts
+++ client/src/app/queries/rekor-search.ts
@@ -1,23 +1,25 @@
 import { useQuery } from "@tanstack/react-query";
-import { useRekorClient } from "@app/pages/Rekor/shared/utils/rekor/api/context";
+import { useRekorBaseUrl, useRekorClient } from "@app/pages/Rekor/shared/utils/rekor/api/context";
 import { type SearchQuery, useRekorSearch } from "@app/pages/Rekor/shared/utils/rekor/api/rekor-api";
 
 export const RekorKey = "Rekor";
 
 export const useFetchRekorEntry = (logIndex: string) => {
   const client = useRekorClient();
+  const [baseUrl] = useRekorBaseUrl();
 
   return useQuery({
-    queryKey: [RekorKey, "entry", logIndex, client.entries],
+    queryKey: [RekorKey, "entry", logIndex, client.entries, baseUrl],
     queryFn: () => client.entries.getLogEntryByIndex({ logIndex: Number(logIndex) }),
   });
 };
 
 export const useFetchRekorSearch = (query: SearchQuery | undefined, page: number) => {
   const search = useRekorSearch();
+  const [baseUrl] = useRekorBaseUrl();
 
   return useQuery({
-    queryKey: [RekorKey, "search", query, page, search],
+    queryKey: [RekorKey, "search", query, page, search, baseUrl],
     queryFn: () => search(query!, page),
     enabled: !!query,
   });
