fd78241fa4bb1eecbb2bd922b6cc5a5d21a7a744
Firas Ghanmi <fghanmi@redhat.com>
2026-07-23 18:14:32 +0200

    feat: surface validFor service window and expiring status in certificates UI
    Signed-off-by: Firas Ghanmi <fghanmi@redhat.com>

--- client/src/app/components/CertificateStatusIcon.tsx
+++ client/src/app/components/CertificateStatusIcon.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import { Icon, type IconComponentProps } from "@patternfly/react-core";
 
-import { CheckCircleIcon, MinusIcon, TimesCircleIcon, WarningTriangleIcon } from "@patternfly/react-icons";
+import { BanIcon, CheckCircleIcon, MinusIcon, TimesCircleIcon, WarningTriangleIcon } from "@patternfly/react-icons";
 
 interface ICertificateStatusIconProps {
   status: string;
@@ -18,16 +18,22 @@ export const CertificateStatusIcon: React.FC<ICertificateStatusIconProps> = ({ s
         <CheckCircleIcon />
       </Icon>
     );
-  } else if (statusLowerCase === "expired") {
-    return (
-      <Icon status="danger" {...iconProps}>
-        <TimesCircleIcon />
-      </Icon>
-    );
   } else if (statusLowerCase === "expiring") {
     return (
       <Icon status="warning" {...iconProps}>
         <WarningTriangleIcon />
+      </Icon>
+    );
+  } else if (statusLowerCase === "revoked") {
+    return (
+      <Icon status="info" {...iconProps}>
+        <BanIcon />
+      </Icon>
+    );
+  } else if (statusLowerCase === "expired") {
+    return (
+      <Icon status="danger" {...iconProps}>
+        <TimesCircleIcon />
       </Icon>
     );
   } else {
